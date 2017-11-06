6c97a49d7fa06b082fd401239a55c963489d7fbb
fernando-sma <fernandosma282@gmail.com>
2017-11-06 14:41:08 -0300

    Primeira versão

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private geolocation: Geolocation) {
 
+  }
+
+  recuperarPosicao(){
+    this.geolocation.getCurrentPosition().then((resp) => {
+      console.log("Latitude", resp.coords.latitude);
+      console.log("Longitude", resp.coords.longitude);
+      alert("Latitude" + resp.coords.latitude + "Longitude" + resp.coords.longitude)
+      })
   }
 
 }
